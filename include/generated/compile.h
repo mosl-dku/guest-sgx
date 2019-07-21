1df73d2d61d5188bc231ae4f072030202575fe09
Hoon94 <dleognsdl1@naver.com>
2019-07-21 21:17:12 +0900

    intentionally inclusion for auto-gen files
    	auto-gen files for analysis

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP Sun Jul 21 20:26:28 KST 2019"
+#define LINUX_COMPILE_BY "mobileos"
+#define LINUX_COMPILE_HOST "yeo14-OptiPlex-7050"
+#define LINUX_COMPILER "gcc version 7.4.0 (Ubuntu 7.4.0-1ubuntu1~18.04.1)"
